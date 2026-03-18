70b73080567fcdec81cf5ba9911c6da1a0ce22b8
Ayman Said <Ayman-Said@live.com>
2026-03-18 03:42:05 +0200

    initial

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const GAS_URL = https://script.google.com/macros/s/AKfycbwgqar-g9-JWAu2FTeElDTH5p2lG3chCpggrBtPeogr7Fz5XHf9owpMkMqKB4VEDSK2/exec;
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbwgqar-g9-JWAu2FTeElDTH5p2lG3chCpggrBtPeogr7Fz5XHf9owpMkMqKB4VEDSK2/exec";
 export const GOOGLE_CLIENT_ID = "482255045821-j3ic9mrmqbsuu7rdql7ujbnfhc4g49vn.apps.googleusercontent.com";
